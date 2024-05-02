5f1d0e4f76ffb04ae66984fec46bd2e8a7438810
Gio W <gio_wan@live.ca>
2024-05-02 12:32:24 -0600

    Add mobile layout

--- src/components/MermaidDash.jsx
+++ src/components/MermaidDash.jsx
@@ -12,20 +12,48 @@ const useStyles = makeStyles(() => ({
     flexDirection: 'row',
     marginTop: '44px',
     flexGrow: 1,
+    margin: '0 -7px',
   },
   filterContainer: {
-    flexGrow: 2,
+    border: '1px solid red',
+    width: '300px',
   },
   mapContainer: {
-    flexGrow: 5,
+    border: '1px solid blue',
+    flexGrow: 1,
   },
   metricsContainer: {
-    flexGrow: 2,
+    border: '1px solid green',
+    width: '300px',
+    position: 'absolute',
+    right: 15,
+    top: '65px',
+    height: 'calc(100vh - 90px)',
+  },
+  '@media (max-width: 600px)': {
+    filterContainer: {
+      border: '1px solid red',
+      width: '80%',
+      position: 'absolute',
+      top: '10%',
+      height: '80%',
+      left: '50%',
+      transform: 'translateX(-50%)',
+    },
+    metricsContainer: {
+      border: '1px solid green',
+      width: '90%',
+      top: 'calc(80% - 45px)',
+      height: '20%',
+      left: '50%',
+      transform: 'translateX(-50%)',
+    },
   },
 }))
 
 export default function MermaidDash() {
   const classes = useStyles()
+
   return (
     <div className={classes.dashContainer}>
       <Header />
