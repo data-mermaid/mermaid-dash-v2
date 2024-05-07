f74708036bb64cdd445ee338e885b5d1317e5ad2
julia-rom <jromanowski@sparkgeo.com>
2024-05-07 11:13:59 -0400

    specify yarn version 4, improve eslint configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,15 +10,12 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'jsx-a11y'],
   rules: {
     'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    'camelcase': 'off',
-    'curly': ['error', 'all'],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    camelcase: 'off',
+    curly: ['error', 'all'],
     'no-underscore-dangle': 'off',
     'react/jsx-props-no-spreading': 'off',
     'react/jsx-boolean-value': 'off',
@@ -27,16 +24,16 @@ module.exports = {
     'no-unused-vars': [
       'error',
       {
-        'varsIgnorePattern': '^_',
-        'argsIgnorePattern': '^_' // ignore unused vars and args that start with _. These vars are unsued, but named for readability/maintainability of code.
-      }
+        varsIgnorePattern: '^_',
+        argsIgnorePattern: '^_', // ignore unused vars and args that start with _. These vars are unsued, but named for readability/maintainability of code.
+      },
     ],
     'no-useless-return': 'off',
     'jsx-a11y/label-has-associated-control': [
       'error',
       {
-        'assert': 'either'
-      }
-    ]
+        assert: 'either',
+      },
+    ],
   },
 }
