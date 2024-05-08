de481df3716d981ac790c29bd311ee38e9dcf93c
julia-rom <jromanowski@sparkgeo.com>
2024-05-08 15:03:33 -0400

    fix import typo

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import macrosPlugin from 'vite-plugin-babel-macro'
+import macrosPlugin from 'vite-plugin-babel-macros'
 
 // https://vitejs.dev/config/
 export default defineConfig({
