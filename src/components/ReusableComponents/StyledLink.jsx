4fb2aec1605fa73d6f8e0b3d6789a05bd6b2d65a
Gio W <gio_wan@live.ca>
2024-05-09 14:22:36 -0600

    Update font family

---
+++ src/components/ReusableComponents/StyledLink.jsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+const StyledLink = styled.a`
+  font-family: 'Open Sans', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif';
+`
+export default StyledLink
