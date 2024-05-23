844de2ca0f172d47a3e3cb9374af93ca6f6b0ef0
Gio W <gio_wan@live.ca>
2024-05-23 08:01:00 -0600

    Add map and pins

--- src/components/LeafletMap.jsx
+++ src/components/LeafletMap.jsx
@@ -1,52 +1,126 @@
-import { useState } from 'react'
+import PropTypes from 'prop-types'
+import { useState, useEffect, useRef, useCallback } from 'react'
 import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import { useLocation, useNavigate } from 'react-router-dom'
+import L from 'leaflet'
+import 'leaflet.markercluster'
+import 'leaflet.markercluster/dist/MarkerCluster.css'
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
 import 'leaflet/dist/leaflet.css'
 import '../customStyles.css'
+import customIcon from '../styles/Icons/map-pin.png'
 
-const defaultMapCenter = [45.4, -75.7]
-const defaultMapZoom = 12
+const defaultMapCenter = [32, -79]
+const defaultMapZoom = 2
 
-export default function LeafletMap() {
-  const params = new URLSearchParams(window.location.search)
-  const paramsMapCenter = [parseFloat(params.get('lat')), parseFloat(params.get('lng'))]
-  const initialMapCenter = paramsMapCenter.every((coord) => !isNaN(coord))
+const selectedIcon = L.icon({
+  iconUrl: customIcon,
+  iconSize: [25, 25],
+  iconAnchor: [12, 10],
+  popupAnchor: [0, 0],
+})
+
+export default function LeafletMap(props) {
+  const { displayedProjects } = props
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  const getURLParams = () => new URLSearchParams(location.search)
+
+  const queryParams = getURLParams()
+  const paramsMapCenter = [queryParams.get('lat'), queryParams.get('lng')]
+  const initialMapCenter = paramsMapCenter.every((coord) => coord !== null)
     ? paramsMapCenter
     : defaultMapCenter
-  const initialMapZoom = parseInt(params.get('zoom')) || defaultMapZoom
+  const initialMapZoom = parseInt(queryParams.get('zoom')) || defaultMapZoom
   const [mapCenter, setMapCenter] = useState(initialMapCenter)
   const [mapZoom, setMapZoom] = useState(initialMapZoom)
+  const markersRef = useRef(null)
 
   function MapEventListener() {
     const map = useMap()
+    const updateURLParams = (queryParams) => {
+      navigate(`${location.pathname}?${queryParams.toString()}`, { replace: true })
+    }
 
     map.on('moveend', () => {
-      // const bounds = map.getBounds()
       const { lat, lng } = map.getCenter()
       const zoom = map.getZoom()
-      // console.log('Bbox:', bounds.toBBoxString())
-
-      const baseUrl =
-        window.location.protocol +
-        '//' +
-        window.location.hostname +
-        (window.location.port ? ':' + window.location.port : '')
-      const url = new URL(baseUrl)
-      const params = new URLSearchParams()
-      params.append('lat', lat)
-      params.append('lng', lng)
-      params.append('zoom', zoom)
-      url.search = params.toString()
-      const finalUrl = url.toString()
-      window.history.pushState({}, '', finalUrl)
+      const queryParams = getURLParams()
+      queryParams.set('lat', lat)
+      queryParams.set('lng', lng)
+      queryParams.set('zoom', zoom)
+      updateURLParams(queryParams)
       setMapCenter([lat, lng])
       setMapZoom(zoom)
     })
 
+    if (!markersRef.current) {
+      markersRef.current = new L.MarkerClusterGroup({
+        spiderfyOnMaxZoom: false,
+      })
+    }
+
+    addNewMarkers()
+    map.addLayer(markersRef.current)
     return null
   }
 
+  const addNewMarkers = useCallback(() => {
+    if (markersRef.current) {
+      displayedProjects.forEach((project) => {
+        project.records.forEach((record) => {
+          const { latitude, longitude, sample_event_id } = record
+          let alreadyExist = false
+          markersRef.current.getLayers().forEach((layer) => {
+            if (layer.options.sample_event_id === sample_event_id) {
+              alreadyExist = true
+            }
+          })
+          if (!alreadyExist) {
+            const customIconMarker = L.marker([latitude, longitude], { icon: selectedIcon }).on(
+              'click',
+              function () {
+                markersRef.current.addLayer(marker)
+                markersRef.current.removeLayer(customIconMarker)
+              },
+            )
+
+            const marker = L.circleMarker([latitude, longitude], {
+              color: 'white',
+              fillColor: 'red',
+              fillOpacity: 1,
+              radius: 8,
+              sample_event_id,
+            }).on('click', function () {
+              markersRef.current.removeLayer(marker)
+              markersRef.current.addLayer(customIconMarker)
+            })
+
+            markersRef.current.addLayer(marker)
+          }
+        })
+      })
+    }
+  }, [displayedProjects, markersRef])
+
+  useEffect(() => {
+    if (markersRef.current) {
+      // Remove any existing markers/sample events that are not in the displayedProjects
+      markersRef.current.getLayers().forEach((layer) => {
+        let exists = displayedProjects.some((project) => {
+          project.records.some((record) => record.sample_event_id === layer.options.sample_event_id)
+        })
+        if (!exists) {
+          markersRef.current.removeLayer(layer)
+        }
+      })
+      addNewMarkers()
+    }
+  }, [displayedProjects, addNewMarkers])
+
   return (
-    <MapContainer center={mapCenter} zoom={mapZoom} scrollWheelZoom={true}>
+    <MapContainer center={mapCenter} zoom={mapZoom} scrollWheelZoom={true} maxZoom={14}>
       <MapEventListener />
       <TileLayer
         url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
@@ -54,4 +128,8 @@ export default function LeafletMap() {
       />
     </MapContainer>
   )
+}
+
+LeafletMap.propTypes = {
+  displayedProjects: PropTypes.array.isRequired,
 }
