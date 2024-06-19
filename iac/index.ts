a2a2a68545ed742258c845b03983ac1258b5ea45
Dustin Sampson <gridcell@users.noreply.github.com>
2024-06-19 12:15:54 -0700

    GitHub workflow and cdk infrastructure setup (#20)
    * cdk for setting up infra
    * GA workflow for deploying

---
+++ iac/index.ts
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+/* eslint-disable no-new */
+import * as cdk from 'aws-cdk-lib'
+import { StaticSiteStack } from './stacks/static-site-stack'
+
+const app = new cdk.App()
+
+const tags = {
+  "Owner": "sysadmin@datamermaid.org",
+}
+
+const subdomain = app.node.tryGetContext('subdomain') || 'dev'
+const domain = app.node.tryGetContext('domain') || 'dashboard3.datamermaid.org'
+
+const cdkEnv = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION || 'us-east-1',
+}
+
+new StaticSiteStack(app, `${subdomain}-dashboard3`, {
+    env: cdkEnv,
+    tags,
+    domainName: domain,
+    siteSubDomain: subdomain
+})
+
+app.synth()
