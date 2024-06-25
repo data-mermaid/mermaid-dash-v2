f657569d56e79d983a914d78e31889669222b7b5
Gio W <gio_wan@live.ca>
2024-06-25 10:46:30 -0600

    Make metrics pane occupy space instead of float

---
+++ src/components/MetricsPane.jsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+const Container = styled('div')`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 20px 0;
+  width: 100%;
+`
+
+const SitesAndTransectsContainer = styled('div')`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+`
+
+export default function MetricsPane(props) {
+  const { displayedProjects } = props
+  const [numSites, setNumSites] = useState(0)
+  const [numTransects, setNumTransects] = useState(0)
+  const [yearRange, setYearRange] = useState('')
+  const [numUniqueCountries, setNumUniqueCountries] = useState(0)
+
+  useEffect(() => {
+    let sites = new Set()
+    let transects = 0
+    let countries = new Set()
+    let years = new Set()
+    displayedProjects.forEach((project) => {
+      project.records.forEach((record) => {
+        countries.add(record.country_name)
+        sites.add(record.site_name)
+        years.add(record.sample_date.split('-')[0])
+      })
+    })
+    let sortedYears = Array.from(years).sort()
+    setNumSites(sites.size)
+    setNumTransects(transects)
+    setNumUniqueCountries(countries.size)
+    if (sortedYears.length === 0) {
+      setYearRange('No data to obtain year range')
+    } else if (sortedYears.length === 1) {
+      setYearRange(`Showing data from ${sortedYears[0]}`)
+    } else {
+      setYearRange(`Showing data from ${sortedYears[0]} to ${sortedYears[sortedYears.length - 1]}`)
+      return
+    }
+  }, [displayedProjects])
+
+  return (
+    <Container>
+      <div>Projects: {displayedProjects.length}</div>
+      <SitesAndTransectsContainer>
+        <div>Sites: {numSites}</div>
+        <div>Transects: {numTransects}</div>
+      </SitesAndTransectsContainer>
+      <div>{yearRange}</div>
+      <div>{numUniqueCountries} countries</div>
+    </Container>
+  )
+}
+
+MetricsPane.propTypes = {
+  displayedProjects: PropTypes.array.isRequired,
+}
