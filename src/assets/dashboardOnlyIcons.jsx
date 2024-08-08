e2a93419b45ac503243a53bbda81f61bdcd5b1d6
Gio W <gio_wan@live.ca>
2024-08-08 14:50:10 -0600

    Add new icons

--- src/assets/dashboardOnlyIcons.jsx
+++ src/assets/dashboardOnlyIcons.jsx
@@ -8,6 +8,9 @@ import personCircle from '@iconify-icons/mdi/person-circle'
 import trayDownload from '@iconify-icons/mdi/tray-download'
 import caretUp from '@iconify-icons/mdi/caret-up'
 import caretDown from '@iconify-icons/mdi/caret-down'
+import calendar from '@iconify-icons/mdi/calendar'
+import text from '@iconify-icons/mdi/text'
+import textBoxMultiple from '@iconify-icons/mdi/text-box-multiple'
 
 export const HamburgerMenu = (props) => <InlineIcon icon={hamburgerMenu} {...props} />
 export const LoginIcon = (props) => <InlineIcon icon={login} {...props} />
@@ -17,3 +20,6 @@ export const IconPersonCircle = (props) => <InlineIcon icon={personCircle} {...p
 export const IconTrayDownload = (props) => <InlineIcon icon={trayDownload} {...props} />
 export const IconCaretUp = (props) => <InlineIcon icon={caretUp} {...props} />
 export const IconCaretDown = (props) => <InlineIcon icon={caretDown} {...props} />
+export const IconCalendar = (props) => <InlineIcon icon={calendar} {...props} />
+export const IconText = (props) => <InlineIcon icon={text} {...props} />
+export const IconTextBoxMultiple = (props) => <InlineIcon icon={textBoxMultiple} {...props} />
