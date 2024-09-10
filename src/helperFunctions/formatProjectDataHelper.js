073163caeb85412b28cfd1342133aba2aeeede79
Gio W <gio_wan@live.ca>
2024-09-10 14:25:57 -0600

    Update logic to support new data structure

--- src/helperFunctions/formatProjectDataHelper.js
+++ src/helperFunctions/formatProjectDataHelper.js
@@ -1,32 +1,37 @@
 export const formatProjectDataHelper = (project) => {
   const { records } = project
   const data = {
-    years: project.years,
-    countries: project.countries.join(', '),
-    organizations: project.organizations.join(', '),
+    years: new Set(),
+    countries: new Set(),
+    organizations: project.tags?.map((tag) => tag.name) || [],
     surveyCount: records.length,
     transects: 0,
   }
 
   records.forEach((record) => {
+    data.years.add(record.sample_date.substring(0, 4))
+    data.countries.add(record.country_name)
     const sumOfSampleUnitCounts = Object.values(record.protocols).reduce((sum, protocol) => {
       return sum + protocol.sample_unit_count
     }, 0)
     data.transects += sumOfSampleUnitCounts
   })
 
+  const years = [...data.years].sort((a, b) => a.localeCompare(b))
   const formattedDateRange =
-    data.years.length === 0
+    years.length === 0
       ? ''
-      : data.years.length === 1
-        ? data.years[0]
-        : `${data.years[0]}-${data.years[data.years.length - 1]}`
+      : years.length === 1
+        ? years[0]
+        : `${years[0]}-${years[years.length - 1]}`
+  const countries = [...data.countries].sort((a, b) => a.localeCompare(b)).join(', ')
+  const organizations = [...data.organizations.sort((a, b) => a.localeCompare(b))].join(', ')
 
   return {
     projectName: project.project_name,
     formattedDateRange,
-    countries: data.countries,
-    organizations: data.organizations,
+    countries,
+    organizations: organizations,
     surveyCount: data.surveyCount,
     transects: data.transects,
   }
