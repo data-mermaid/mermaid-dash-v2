bc19871feb830e314b38806c532a7abc6c66b3cd
m n <mnunes@sparkgeo.com>
2024-09-20 16:21:51 -0600

    Previously missed linting

--- src/components/MapAndTableControls/MapAndTableControls.jsx
+++ src/components/MapAndTableControls/MapAndTableControls.jsx
@@ -91,29 +91,6 @@ const MapAndTableControls = ({ map = undefined, view, setView }) => {
     map.fitBounds(bounds)
   }
 
-  const handleZoomToSelectedSite = () => {
-    if (!map) {
-      return
-    }
-
-    if (queryParams.has('sample_event_id')) {
-      const sample_event_id = queryParams.get('sample_event_id')
-      const foundSampleEvent = displayedProjects
-        .flatMap((project) => project.records)
-        .find((record) => record.sample_event_id === sample_event_id)
-      if (!foundSampleEvent) {
-        return
-      }
-      const { latitude, longitude } = foundSampleEvent
-      map.setCenter([longitude, latitude])
-      map.setZoom(18)
-    }
-  }
-
-  const hasSelectedSite = () => {
-    return queryParams.has('sample_event_id')
-  }
-
   return (
     <ControlContainer>
       {isAnyActiveFilters() ? (
