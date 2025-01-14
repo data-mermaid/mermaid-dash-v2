bf529775d4e8c3c46c732776bc10307d27c14e9c
tienhoah <nickhoang11@gmail.com>
2025-01-14 14:59:31 -0800

    Roll back filter project that have no records.

--- src/context/FilterProjectsContext.jsx
+++ src/context/FilterProjectsContext.jsx
@@ -278,6 +278,12 @@ export const FilterProjectsProvider = ({ children }) => {
             records: project.records.filter((record) => isRecordWithinMapBounds(record, mapBbox)),
           }
         })
+        .filter((project) => {
+          // Filter out projects that have no records
+          let projectHasRecords = !isAnyActiveFilters() || project.records.length > 0
+          const isProjectVisible = projectHasRecords || showProjectsWithNoRecords
+          return isProjectVisible
+        })
     },
     [
       projectData.results,
