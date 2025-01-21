86dafd056fa84c6c126c7d19fa12c9f2b2eebb53
tienhoah <nickhoang11@gmail.com>
2025-01-20 17:41:21 -0800

    Remove commented code.

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,13 +2,11 @@ import { useState, useEffect } from 'react'
 
 const useLocalStorage = (key, defaultValue) => {
   const [state, setState] = useState(() => {
-    // Get initial value from localStorage or use default
     const storedValue = localStorage.getItem(key)
     return storedValue !== null ? JSON.parse(storedValue) : defaultValue
   })
 
   useEffect(() => {
-    // Update localStorage whenever state changes
     localStorage.setItem(key, JSON.stringify(state))
   }, [key, state])
 
