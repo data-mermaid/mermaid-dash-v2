820f42b03537e9598d933e468d4f5637121e09dc
tienhoah <6288799+tienhoah@users.noreply.github.com>
2025-03-03 08:35:32 -0800

    [M1266] Preserve plot selection in query params
    * Refactor MetricsPaneChartTabs to use URL parameters for tab selection and update chart ID to lowercase.

--- src/components/MetricsPane/MetricsPaneChartTabs.jsx
+++ src/components/MetricsPane/MetricsPaneChartTabs.jsx
@@ -1,14 +1,48 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import PropTypes from 'prop-types'
+import { FilterProjectsContext } from '../../context/FilterProjectsContext'
 
 import { ChartTabsWrapper, Tab, TabList, TabPanel } from './MetricsPaneChartTabs.styles'
+import { URL_PARAMS } from '../../constants/constants'
+
+const { BENTHIC_COVER, FISH_BIOMASS, BLEACHING, HABITAT_COMPLEXITY } = URL_PARAMS
+
 const TAB_IDS = {
   AGGREGATE: 'aggregate',
   TIME_SERIES: 'timeSeries',
 }
 
+const getChartParamTypeByChartId = {
+  'hard-coral-cover': BENTHIC_COVER,
+  'fish-biomass': FISH_BIOMASS,
+  bleaching: BLEACHING,
+  'habitat-complexity': HABITAT_COMPLEXITY,
+}
+
+const useTabSelection = (chartQueryParam) => {
+  const { getURLParams, updateURLParams } = useContext(FilterProjectsContext)
+  const queryParams = getURLParams()
+  const [selectedTab, setSelectedTab] = useState(
+    queryParams.has(chartQueryParam) ? TAB_IDS.TIME_SERIES : TAB_IDS.AGGREGATE,
+  )
+
+  const handleSelectedTabChange = (tabId) => {
+    if (tabId === TAB_IDS.TIME_SERIES) {
+      queryParams.set(chartQueryParam, TAB_IDS.TIME_SERIES)
+    } else {
+      queryParams.delete(chartQueryParam)
+    }
+
+    updateURLParams(queryParams)
+    setSelectedTab(tabId)
+  }
+
+  return { selectedTab, handleSelectedTabChange }
+}
+
 export const MetricsPaneChartTabs = ({ id, aggregatePanelContent, timeSeriesPanelContent }) => {
-  const [selectedTab, setSelectedTab] = useState(TAB_IDS.AGGREGATE)
+  const chartQueryParam = getChartParamTypeByChartId[id]
+  const { selectedTab, handleSelectedTabChange } = useTabSelection(chartQueryParam)
   const tabsPanelAggregateId = `${id}-tabpanel-aggregate`
   const tabsPanelTimeSeriesId = `${id}-tabpanel-time-series`
   const isAggregateSelected = selectedTab === TAB_IDS.AGGREGATE
@@ -23,7 +57,7 @@ export const MetricsPaneChartTabs = ({ id, aggregatePanelContent, timeSeriesPane
           id={TAB_IDS.AGGREGATE}
           aria-selected={isAggregateSelected}
           aria-controls={tabsPanelAggregateId}
-          onClick={() => setSelectedTab(TAB_IDS.AGGREGATE)}
+          onClick={() => handleSelectedTabChange(TAB_IDS.AGGREGATE)}
           $tabPosition="left"
         >
           Aggregate
@@ -34,7 +68,7 @@ export const MetricsPaneChartTabs = ({ id, aggregatePanelContent, timeSeriesPane
           id={TAB_IDS.TIME_SERIES}
           aria-selected={isTimeSeriesSelected}
           aria-controls={tabsPanelTimeSeriesId}
-          onClick={() => setSelectedTab(TAB_IDS.TIME_SERIES)}
+          onClick={() => handleSelectedTabChange(TAB_IDS.TIME_SERIES)}
           $tabPosition="right"
         >
           Time Series
