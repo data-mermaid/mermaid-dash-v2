8cfe9566089e1ca89e59a781cb9ae9f94b472616
tienhoah <nickhoang11@gmail.com>
2025-03-20 11:47:09 -0700

    Prevent toFixed error for sample event benthic plot.

--- src/components/MetricsPane/charts/SampleEventBenthicPlot.jsx
+++ src/components/MetricsPane/charts/SampleEventBenthicPlot.jsx
@@ -25,18 +25,14 @@ export const SampleEventBenthicPlot = ({ benthicType, benthicData }) => {
 
   const plotlyDataConfiguration = benthicPercentageCover
     .filter(({ value }) => value > 0)
-    .map(({ name, value, color }) => {
-      return {
-        x: [1],
-        y: [value],
-        type: 'bar',
-        name: `${name} (${value.toFixed(1)}%)`,
-        marker: {
-          color: color,
-        },
-        hovertemplate: `${name}<br>%{y:.1f}% cover<extra></extra>`,
-      }
-    })
+    .map(({ name, value, color }) => ({
+      x: [1],
+      y: [value],
+      type: 'bar',
+      name: `${name} (${(value ?? 0).toFixed(1)}%)`,
+      marker: { color },
+      hovertemplate: `${name}<br>%{y:.1f}% cover<extra></extra>`,
+    }))
 
   const plotlyLayoutConfiguration = {
     ...chartTheme.layout,
