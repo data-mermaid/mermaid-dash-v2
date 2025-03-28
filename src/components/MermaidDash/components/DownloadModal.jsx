853f4fc2d86d5829f375c87e77e961fe6fec4b43
tienhoah <nickhoang11@gmail.com>
2025-03-27 17:17:31 -0700

    Refactor modals to improve layout and responsiveness; update media queries and table components

--- src/components/MermaidDash/components/DownloadModal.jsx
+++ src/components/MermaidDash/components/DownloadModal.jsx
@@ -280,8 +280,6 @@ const DownloadModal = ({ isOpen, onDismiss, selectedMethod, handleSelectedMethod
       onDismiss={onDismiss}
       footerContent={footerContent}
       contentOverflowIsVisible={true}
-      modalCustomWidth={modalMode === 'download' ? '1150px' : '600px'}
-      modalCustomHeight={modalMode === 'download' ? '600px' : '200px'}
     />
   )
 }
