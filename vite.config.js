0d712fe52b5ac3415e50a2b48753cb37a862a90a
ms280690 <mehul@sparkgeo.com>
2026-06-05 16:49:01 -0700

    add sentry monitoring
    Signed-off-by: ms280690 <mehul@sparkgeo.com>

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import i18nextLoader from 'vite-plugin-i18next-loader'
+import { sentryVitePlugin } from '@sentry/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,22 @@ export default defineConfig({
       'top-level-await': true,
     },
   },
-  plugins: [react(), svgr(), i18nextLoader({ paths: ['./src/locales'] })],
+  plugins: [
+    react(),
+    svgr(),
+    i18nextLoader({ paths: ['./src/locales'] }),
+    // Only upload source maps during CI builds where SENTRY_AUTH_TOKEN is set
+    ...(process.env.SENTRY_AUTH_TOKEN
+      ? [
+          sentryVitePlugin({
+            org: process.env.SENTRY_ORG,
+            project: process.env.SENTRY_PROJECT,
+            authToken: process.env.SENTRY_AUTH_TOKEN,
+          }),
+        ]
+      : []),
+  ],
+  build: {
+    sourcemap: true,
+  },
 })
