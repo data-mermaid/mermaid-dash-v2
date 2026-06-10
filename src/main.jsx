531b9b36e88d2751a32a4d7305778a00c0afbb55
Mehul Solanki <mehul@sparkgeo.com>
2026-06-10 10:40:01 -0700

    add sentry monitoring (#233)
    * add sentry monitoring
    
    Signed-off-by: ms280690 <mehul@sparkgeo.com>
    
    * address PR comments
    
    Signed-off-by: ms280690 <mehul@sparkgeo.com>
    
    * address PR comments
    
    Signed-off-by: ms280690 <mehul@sparkgeo.com>
    
    ---------
    
    Signed-off-by: ms280690 <mehul@sparkgeo.com>

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import React from 'react'
 import { PostHogProvider } from 'posthog-js/react'
+import { initSentry, Sentry } from './sentry.js'
+
+initSentry()
 
 const options = {
   api_host: import.meta.env.VITE_ENVIRONMENT === 'prod' ? import.meta.env.VITE_PUBLIC_POSTHOG_HOST : null,
@@ -11,16 +14,18 @@ const options = {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {import.meta.env.VITE_PUBLIC_POSTHOG_HOST && import.meta.env.VITE_ENVIRONMENT === 'prod'? (
-      <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
+    <Sentry.ErrorBoundary fallback={<p>An unexpected error has occurred.</p>}>
+      {import.meta.env.VITE_PUBLIC_POSTHOG_HOST && import.meta.env.VITE_ENVIRONMENT === 'prod' ? (
+        <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </PostHogProvider>
+      ) : (
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </PostHogProvider>
-    ) : (
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    )}
+      )}
+    </Sentry.ErrorBoundary>
   </React.StrictMode>,
 )
